7efd285bd9736a3599705faffcb8e52203d25b3d
Mark Fisher <markfisher@vmware.com>
2012-07-10 18:19:47 -0400

    added executor change to control bus sample

--- siia-examples-parent/monitoring/src/main/java/siia/monitoring/controlbus/ControlBusDemo.java
+++ siia-examples-parent/monitoring/src/main/java/siia/monitoring/controlbus/ControlBusDemo.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.integration.Message;
 import org.springframework.integration.MessageChannel;
 import org.springframework.integration.support.MessageBuilder;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 public class ControlBusDemo {
 
@@ -12,10 +13,14 @@ public class ControlBusDemo {
 		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml", ControlBusDemo.class);
 		NumberHolder numberHolder = context.getBean("numberHolder", NumberHolder.class);
 		MessageChannel controlChannel = context.getBean("controlChannel", MessageChannel.class);
-		System.out.println("value before: " + numberHolder.getNumber());
+		System.out.println("number before increment: " + numberHolder.getNumber());
 		Message<String> message = MessageBuilder.withPayload("@numberHolder.increment()").build();
 		controlChannel.send(message);
-		System.out.println("value after:  " + numberHolder.getNumber());
+		System.out.println("number after increment:  " + numberHolder.getNumber());
+		ThreadPoolTaskExecutor executor = context.getBean("myExecutor", ThreadPoolTaskExecutor.class);
+		System.out.println("max pool size before update: " + executor.getMaxPoolSize());
+		controlChannel.send(MessageBuilder.withPayload("@myExecutor.setMaxPoolSize(25)").build());
+		System.out.println("max pool size after update:  " + executor.getMaxPoolSize());
 	}
 
 }
