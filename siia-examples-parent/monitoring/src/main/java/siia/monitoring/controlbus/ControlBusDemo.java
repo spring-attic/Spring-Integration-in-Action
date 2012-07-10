d782ffa261f509253bb3370f84aedfb8cbdbdfa3
Mark Fisher <markfisher@vmware.com>
2012-07-10 18:36:35 -0400

    added Lifecycle to control bus sample

--- siia-examples-parent/monitoring/src/main/java/siia/monitoring/controlbus/ControlBusDemo.java
+++ siia-examples-parent/monitoring/src/main/java/siia/monitoring/controlbus/ControlBusDemo.java
@@ -17,6 +17,12 @@ public class ControlBusDemo {
 		Message<String> message = MessageBuilder.withPayload("@numberHolder.increment()").build();
 		controlChannel.send(message);
 		System.out.println("number after increment:  " + numberHolder.getNumber());
+		FilePoller filePoller = context.getBean("filePoller", FilePoller.class);
+		System.out.println("file poller isRunning before start: " + filePoller.isRunning());
+		controlChannel.send(MessageBuilder.withPayload("@filePoller.start()").build());
+		System.out.println("file poller isRunning after start:  " + filePoller.isRunning());
+		controlChannel.send(MessageBuilder.withPayload("@filePoller.stop()").build());
+		System.out.println("file poller isRunning after stop:   " + filePoller.isRunning());
 		ThreadPoolTaskExecutor executor = context.getBean("myExecutor", ThreadPoolTaskExecutor.class);
 		System.out.println("max pool size before update: " + executor.getMaxPoolSize());
 		controlChannel.send(MessageBuilder.withPayload("@myExecutor.setMaxPoolSize(25)").build());
---
+++ siia-examples-parent/monitoring/src/main/java/siia/monitoring/controlbus/FilePoller.java
@@ -0,0 +1,25 @@
+package siia.monitoring.controlbus;
+
+import org.springframework.context.Lifecycle;
+
+/**
+ * This does not poll files. It simply demonstrates that any
+ * Lifecycle implementation can be managed via the control bus.
+ */
+public class FilePoller implements Lifecycle {
+
+	private volatile boolean running = false;
+
+	public void start() {
+		this.running = true;
+	}
+
+	public void stop() {
+		this.running = false;
+	}
+
+	public boolean isRunning() {
+		return this.running;
+	}
+
+}
