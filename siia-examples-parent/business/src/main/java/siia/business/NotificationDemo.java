d459f43b9355cb594ddc4be52fb34bfdbb8aa42e
Mark Fisher <markfisher@vmware.com>
2012-08-04 18:39:40 -0400

    added NotificationDemo for local verification

---
+++ siia-examples-parent/business/src/main/java/siia/business/NotificationDemo.java
@@ -0,0 +1,15 @@
+package siia.business;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class NotificationDemo {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("notificationPublisher.xml");
+		FlightStatusNotificationPublisher publisher = context.getBean(FlightStatusNotificationPublisher.class);
+		FlightStatusEvent event = new FlightStatusEvent();
+		publisher.publishNotification(event);
+	}
+
+}
