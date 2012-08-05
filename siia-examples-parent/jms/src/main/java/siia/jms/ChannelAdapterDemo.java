cc2ea956bff7200649720c05cc56b14f2d38330e
Mark Fisher <markfisher@vmware.com>
2012-08-05 17:14:17 -0400

    added channel adapters echo example

---
+++ siia-examples-parent/jms/src/main/java/siia/jms/ChannelAdapterDemo.java
@@ -0,0 +1,21 @@
+package siia.jms;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.core.MessagingTemplate;
+import org.springframework.integration.core.PollableChannel;
+
+public class ChannelAdapterDemo {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("siia/jms/channel-adapters.xml");
+		MessageChannel toJMS = context.getBean("toJMS", MessageChannel.class);
+		PollableChannel fromJMS = context.getBean("fromJMS", PollableChannel.class);
+		MessagingTemplate template = new MessagingTemplate();
+		template.convertAndSend(toJMS, "echo");
+		Object response = template.receiveAndConvert(fromJMS);
+		System.out.println("response: " + response);
+	}
+
+}
