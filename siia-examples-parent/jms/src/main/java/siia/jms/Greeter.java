4e6b29bc78282254bebfef02fdf58321fb6bb71f
Mark Fisher <markfisher@vmware.com>
2012-08-05 17:04:19 -0400

    added MDP example

---
+++ siia-examples-parent/jms/src/main/java/siia/jms/Greeter.java
@@ -0,0 +1,8 @@
+package siia.jms;
+
+public class Greeter {
+
+	public void sayHello(String name) {
+		System.out.println("Hello " + name);
+	}
+}
---
+++ siia-examples-parent/jms/src/main/java/siia/jms/MessageDrivenPojoDemo.java
@@ -0,0 +1,18 @@
+package siia.jms;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+
+public class MessageDrivenPojoDemo {
+
+	public static void main(String[] args) {
+		// start the context that contains the message-driven POJO
+		ApplicationContext context = new ClassPathXmlApplicationContext("siia/jms/message-driven-pojo.xml");
+
+		// send a Message with JmsTemplate
+		JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+		jmsTemplate.convertAndSend("siia.mdp.queue", "World");
+	}
+
+}
