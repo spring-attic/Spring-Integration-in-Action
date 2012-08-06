5cc3460d367c6f300dd0a4726481999df49b238d
Mark Fisher <markfisher@vmware.com>
2012-08-05 23:28:11 -0400

    added twitter sample

---
+++ siia-examples-parent/twitter/src/main/java/siia/twitter/SearchToLog.java
@@ -0,0 +1,11 @@
+package siia.twitter;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SearchToLog {
+
+	public static void main(String[] args) {
+		new ClassPathXmlApplicationContext("siia/twitter/search-to-log.xml");
+	}
+
+}
